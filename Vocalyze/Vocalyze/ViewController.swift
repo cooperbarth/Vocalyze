7ac319913075639ac043ee1638865904b8d4e396
Cooper Barth <cooperfbarth@gmail.com>
2019-03-29 03:46:10 -0400

    starting over

---
+++ Vocalyze/Vocalyze/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vocalyze
+//
+//  Created by Cooper Barth on 3/29/19.
+//  Copyright © 2019 cooperbarth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- Vocalyze/Vocalyze/Vocalyze-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Vocalyze-Bridging-Header.h
-//  Vocalyze
-//
-//  Created by Cooper Barth on 3/29/19.
-//  Copyright © 2019 cooperbarth. All rights reserved.
-//
-
-#import <SpotifyiOS/SpotifyiOS.h>
